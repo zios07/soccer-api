1a8f7a94b4f6f172e9096a7d4dda4746bb4618f7
Zakaria Kaoukab <Zkaoukab@omnidata.local>
2018-06-13 15:42:12 +0000

    fix participation quite

--- src/main/java/com/soccer/service/impl/ParticipationService.java
+++ src/main/java/com/soccer/service/impl/ParticipationService.java
@@ -16,39 +16,42 @@ import java.util.List;
 public class ParticipationService implements IParticipationService {
 
 
-  @Autowired
-  private ParticipationRepository participationRepository;
+    @Autowired
+    private ParticipationRepository participationRepository;
 
-  @Autowired
-  private MatchRepository matchRepository;
+    @Autowired
+    private MatchRepository matchRepository;
 
 
-  @Override
-  public Participation findByPlayerIdAndMatchId(Long playerId, Long matchId) {
-    return participationRepository.findByPlayerIdAndMatchId(playerId, matchId);
-  }
-
-  @Override
-  public List<Participation> findByPlayerId(Long playerId) {
-    return participationRepository.findByPlayerId(playerId);
-  }
-
-  @Override
-  public Participation create(Match m, Long playerId) {
-
-    Participation participation = new Participation(m, playerId);
-    participation = participationRepository.save(participation);
-      Match match = matchRepository.findOne(m.getId());
-      match.setAvailablePlaces(match.getAvailablePlaces() - 1);
-      matchRepository.save(match);
-    return participation;
-  }
-
-  @Override
-  public void delete(Long playerId, Long matchId) {
-    Participation participation = participationRepository.findByPlayerIdAndMatchId(playerId, matchId);
-    if(participation != null) {
-      participationRepository.delete(participation.getId());
+    @Override
+    public Participation findByPlayerIdAndMatchId(Long playerId, Long matchId) {
+        return participationRepository.findByPlayerIdAndMatchId(playerId, matchId);
     }
-  }
+
+    @Override
+    public List<Participation> findByPlayerId(Long playerId) {
+        return participationRepository.findByPlayerId(playerId);
+    }
+
+    @Override
+    public Participation create(Match m, Long playerId) {
+
+        Participation participation = new Participation(m, playerId);
+        participation = participationRepository.save(participation);
+        Match match = matchRepository.findOne(m.getId());
+        match.setAvailablePlaces(match.getAvailablePlaces() - 1);
+        matchRepository.save(match);
+        return participation;
+    }
+
+    @Override
+    public void delete(Long playerId, Long matchId) {
+        Participation participation = participationRepository.findByPlayerIdAndMatchId(playerId, matchId);
+        if (participation != null) {
+            participationRepository.delete(participation.getId());
+        }
+        Match match = matchRepository.findOne(matchId);
+        match.setAvailablePlaces(match.getAvailablePlaces() + 1);
+        matchRepository.save(match);
+    }
 }
